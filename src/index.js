4986bf3b51f691e50049f7ec87154a1ecf24f2ad
Giovanni Robira <giovanni.robira@accenture.com>
2019-08-20 15:13:17 -0300

    Logger prod and dev

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 require('./express-server');
+require('./logger');
--- src/logger.js
+++ src/logger.js
@@ -1,8 +1,30 @@
 import winston from 'winston';
 
-const logger = winston.createLogger({
+const loggerDev = winston.createLogger({
   level: process.env.LOG_LEVEL,
-  format: winston.format.json(),
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.simple()
+  ),
+  transports: [new winston.transports.Console()],
 });
+
+const loggerProd = winston.createLogger({
+  level: process.env.LOG_LEVEL,
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [new winston.transports.Console()],
+});
+
+const logger = process.env.NODE_ENV == 'prod' ? loggerProd : loggerDev;
+
+logger.info('Log info');
+logger.error('Log error');
+logger.warn('Log warn');
+logger.debug('Log debug');
+logger.silly('Log silly');
+logger.verbose('Log berbose');
 
 export default logger;
